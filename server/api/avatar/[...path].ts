790560431bbba8da794c1a2309f740d69908cf72
legendofzer0 <jenishmhrz123@gmail.com>
2025-11-14 13:49:54 +0545

    chore: remove unused package

--- server/api/avatar/[...path].ts
+++ server/api/avatar/[...path].ts
@@ -3,14 +3,11 @@ import { createReadStream, existsSync } from "fs";
 import { join } from "path";
 
 export default defineEventHandler(async (event) => {
-	console.log("reached here");
-
 	const pathParam = event.context.params?.path;
 
 	const filePath = Array.isArray(pathParam)
 		? join(process.cwd(), "server/files/userAvatar/", ...pathParam)
 		: join(process.cwd(), "server/files/userAvatar/", pathParam || "");
-	console.log(filePath);
 	if (!existsSync(filePath)) {
 		throw createError({
 			statusCode: 404,
